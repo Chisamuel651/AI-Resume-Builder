81aa582fb2cbe639fcab95ab7fe3f4bfccc62ce5
ChiSamuel651 <chijoe0306@gmail.com>
2024-12-04 00:52:36 +0100

    Completion of PersonalDetail.jsx configuration for the resume

--- src/dashboard/resume/components/form/PersonalDetail.jsx
+++ src/dashboard/resume/components/form/PersonalDetail.jsx
@@ -1,13 +1,29 @@
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { ResumeInfoContext } from '@/context/ResumeInfoContext'
-import React, { useContext } from 'react'
+import { LoaderCircle } from 'lucide-react'
+import React, { useContext, useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import GlobalApi from './../../../../../service/GlobalApi'
+import { toast } from 'sonner'
 
 function PersonalDetail({enableNext}) {
+    const params = useParams();
     const {resumeInfo, setResumeInfo} = useContext(ResumeInfoContext)
+    const [ formData, setFormData ] = useState()
+    const [ loading, setLoading ] = useState(false)
+
+    useEffect(() => {
+        console.log(params)
+    }, [])
     const handleInputChange = (e) => {
         enableNext(false);
         const {name, value} = e.target;
+
+        setFormData({
+            ...formData,
+            [name]:value
+        })
 
         setResumeInfo({
             ...resumeInfo,
@@ -16,7 +32,19 @@ function PersonalDetail({enableNext}) {
     }
     const onSave = (e) => {
         e.preventDefault();
-        enableNext(true)
+        setLoading(true)
+        const data = {
+            data: formData
+        }
+        GlobalApi.UpdateResumeDetail(params?.resumeId, data).then(resp => {
+            console.log(resp)
+            enableNext(true)
+            setLoading(false)
+            toast("Detail updated.")
+        }, (error) => {
+            setLoading(false)
+        })
+        
     }
   return (
     <div className='p-5 shadow-lg rounded-lg border-t-primary border-t-4 mt-10'>
@@ -27,33 +55,35 @@ function PersonalDetail({enableNext}) {
         <div className='grid grid-cols-2 mt-5 gap-3'>
             <div>
                 <label className='text-sm'>First Name</label>
-                <Input name='firstName' required onChange={handleInputChange} />
+                <Input name='firstName' defaultValue={resumeInfo.firstName} required onChange={handleInputChange} />
             </div>
             <div>
                 <label className='text-sm'>Last Name</label>
-                <Input name='lastName' required onChange={handleInputChange} />
+                <Input name='lastName' defaultValue={resumeInfo.lastName} required onChange={handleInputChange} />
             </div>
             <div className='col-span-2'>
                 <label className='text-sm'>Job Title</label>
-                <Input name='jobTitle' required onChange={handleInputChange} />
+                <Input name='jobTitle' defaultValue={resumeInfo.jobTitle} required onChange={handleInputChange} />
             </div>
             <div className='col-span-2'>
                 <label className='text-sm'>Address</label>
-                <Input name='address' required onChange={handleInputChange} />
+                <Input name='address' defaultValue={resumeInfo.address} required onChange={handleInputChange} />
             </div>
             <div>
                 <label className='text-sm'>Phone</label>
-                <Input name='phone' type='number' required onChange={handleInputChange} />
+                <Input name='phone' defaultValue={resumeInfo.phone} type='number' required onChange={handleInputChange} />
             </div>
             <div>
                 <label className='text-sm'>Email</label>
-                <Input name='email' type='email' required onChange={handleInputChange} />
+                <Input name='email' defaultValue={resumeInfo.email} type='email' required onChange={handleInputChange} />
                 
             </div>
         </div>
 
         <div className='mt-3 flex justify-end'>
-            <Button type='submit'>Save</Button>
+            <Button type='submit' disabled={loading}>
+                {loading ? <LoaderCircle className='animate-spin' />: 'Save'}
+            </Button>
         </div>
       </form>
     </div>
